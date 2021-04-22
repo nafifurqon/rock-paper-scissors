342c6587043243e79880ab08e009d950fad4be75
nafifurqon <muhammadnafi98@gmail.com>
2021-04-22 14:54:51 +0700

    changed port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || '4001';
 const gamesRouter = require('./routes/games');
 const userRouter = require('./routes/user');
 
